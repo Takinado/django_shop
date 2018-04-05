a3833325bd42f5ed4e1a22e829eb4c53fb5222a8
Takinado <takinado@mail.ru>
2018-04-06 00:02:01 +0300

    Added product and product category models

---
+++ shop/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ['-name']
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name='Категория')
+    name = models.CharField(max_length=200, db_index=True, verbose_name='Название')
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d/', blank=True, verbose_name='Изображение товара')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    price = models.DecimalField(max_digits=10, decimal_places=0, verbose_name='Цена')
+    stock = models.BooleanField(default=True, verbose_name='На складе')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
+    updated = models.DateTimeField(auto_now=True, verbose_name='Обновлён')
+
+    class Meta:
+        ordering = ['-name']
+        index_together = [
+            ['id', 'slug']
+        ]
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+
+    def __str__(self):
+        return self.name
