b7f3917a6c169a74877ce465d8f170b326133bf7
Takinado <takinado@mail.ru>
2018-04-05 12:17:58 +0300

    Configuring the PostgreSQL Database

--- django_shop/settings.py
+++ django_shop/settings.py
@@ -64,16 +64,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_shop.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
