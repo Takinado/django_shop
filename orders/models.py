20edf1a3247ac8e150f1feebe239a04663568ff1
Takinado <takinado@mail.ru>
2018-04-17 18:37:08 +0300

    Add cupon to order

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,9 @@
+from _decimal import Decimal
+
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
+from cupons.models import Cupon
 from shop.models import Product
 
 
@@ -10,12 +14,16 @@ class Order(models.Model):
     address = models.CharField(max_length=250, verbose_name='Адрес')
     postal_code = models.CharField(max_length=20, verbose_name='Почтовый код')
     city = models.CharField(max_length=100, verbose_name='Город')
+    cupon = models.ForeignKey(Cupon, on_delete=models.PROTECT, related_name='orders', null=True, blank=True,
+                              verbose_name='Купон')
+    discount = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)],
+                                   verbose_name='Скидка')
     created = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
     updated = models.DateTimeField(auto_now=True, verbose_name='Обновлён')
     paid = models.BooleanField(default=False, verbose_name='Оплачен')
 
     class Meta:
-        ordering = ('-created', )
+        ordering = ('-created',)
         verbose_name = 'Заказ'
         verbose_name_plural = 'Заказы'
 
@@ -23,7 +31,8 @@ class Order(models.Model):
         return 'Заказ: {}'.format(self.id)
 
     def get_total_cost(self):
-        return sum(item.get_cost() for item in self.items.all())
+        total_cost = sum(item.get_cost() for item in self.items.all())
+        return total_cost - total_cost * (self.discount / Decimal('100'))
 
 
 class OrderItem(models.Model):
